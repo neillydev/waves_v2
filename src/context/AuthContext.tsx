1410f84718588feffe4ebb0a26d21fbf83aeec8c
neillydev <neillydev@gmail.com>
2023-03-10 20:48:39 -0800

    create Auth context

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,27 @@
+import React, {useMemo, createContext, useReducer} from "react";
+
+export const AuthContext = createContext<any| undefined>(undefined);
+
+const initialState = false;
+const reducer = (state: boolean, action: any) => {
+  switch(action.type) {
+    case 'true':
+        return true;
+    case 'false':
+      return false;
+    default:
+        return state;
+  }
+};
+
+export const AuthProvider = (props: any) => {
+  const [authState, authDispatch] = useReducer(reducer, initialState);
+  const contextValue = useMemo(() => {
+    return { authState, authDispatch };
+  }, [authState, authDispatch]);
+  return (
+    <AuthContext.Provider value={contextValue}>
+        {props.children}
+    </AuthContext.Provider>
+  );
+}
