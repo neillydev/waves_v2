9eaff240739a4f9c78eb71b861c1a33473e3e213
neillydev <neillydev@gmail.com>
2023-03-15 02:33:48 -0700

    add Oswald font

--- src/components/ContainerBlock/ContainerBlock.tsx
+++ src/components/ContainerBlock/ContainerBlock.tsx
@@ -22,6 +22,7 @@ export default function ContainerBlock({ children, ...customMeta }: any) {
         <style>
           @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
           @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+          @import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap');
         </style>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="robots" content="follow, index" />
