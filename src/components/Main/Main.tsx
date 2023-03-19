d1b620e7a64f1d2fcfb9524607e5ffda8bdf98f7
neillydev <neillydev@gmail.com>
2023-03-18 17:31:07 -0700

    add styling changes to Main component

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -10,48 +10,51 @@ import Trend from '../Trend/Trend';
 const Main = () => {
   return (
     <div className={styles.mainWrapper}>
-        <div className={styles.mainLeft}>
-          <div className={styles.mainLeftContainer}>
-            <div className={styles.mainLeftWrapper}>
-              <div className={styles.leftMain}>
-                <SideItem icon={<FireSVG />} header='Trending' subHeader='' path='/' />
-                <SideItem icon={<FollowersSVG />} header='Following' subHeader=''path='/following' />
-              </div>
-              <div className={styles.separator} />
-              <div className={styles.leftLogin}>
-                <h2>Login to like videos, interact with creators, and see comments</h2>
-                <button>Login</button>
-              </div>
-              <div className={styles.separator} />
-              <div className={styles.leftFeatured}>
-                <h2 className={styles.leftTitle}>Featured Creators</h2>
-                <SideItem icon="" header='Vernon Neilly III' subHeader='@neillydev' path='/' />
-                <SideItem icon="" header='Pistachio The Cat' subHeader='@pistachio' path='/' />
-              </div>
-              <div className={styles.separator} />
-              <div className={styles.leftSurf}>
-                <h2 className={styles.leftTitle}>Surf Trends</h2>
+      <div className={styles.mainLeft}>
+        <div className={styles.mainLeftContainer}>
+          <div className={styles.mainLeftWrapper}>
+            <div className={styles.leftMain}>
+              <SideItem icon={<FireSVG />} header='Trending' subHeader='' path='/' />
+              <SideItem icon={<FollowersSVG />} header='Following' subHeader='' path='/following' />
+            </div>
+            <div className={styles.separator} />
+            <div className={styles.leftLogin}>
+              <h2>Login to like videos, interact with creators, and see comments</h2>
+              <button>Login</button>
+            </div>
+            <div className={styles.separator} />
+            <div className={styles.leftSection}>
+              <h2 className={styles.leftTitle}>Featured Creators</h2>
+              <SideItem icon="" header='Vernon Neilly III' subHeader='@neillydev' path='/' />
+              <SideItem icon="" header='Pistachio The Cat' subHeader='@pistachio' path='/' />
+            </div>
+            <div className={styles.separator} />
+            <div className={styles.leftSection}>
+              <h2 className={styles.leftTitle}>Surf Trends</h2>
+              <div className={styles.trendsWrapper}>
                 <Trend title='waves' />
+                <Trend title='surf' />
               </div>
-              <div className={styles.separator} />
-              <div className={styles.leftFooter}></div>
             </div>
-            <div className={styles.mainLeftScroll}>
+            <div className={styles.separator} />
+            <div className={styles.leftSection}></div>
+          </div>
+          <div className={styles.mainLeftScroll}>
 
-            </div>
           </div>
         </div>
-        <div className={styles.mainRight}>
-          <div className={styles.mainRightWrapper}>
-            <Post profileImg='https://images.unsplash.com/photo-1615789591457-74a63395c990?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZG9tZXN0aWMlMjBjYXR8ZW58MHx8MHx8&w=1000&q=80'
-              username='pistachio' 
-              name='Pistachio The Cat' 
-              mediaSrc='https://surfwaves.b-cdn.net/cat_video_example.mp4' 
-              caption='Pistachio moves in silence #pistachio #trending #cats #waves' 
-              soundCaption='pistachio' 
-              soundSrc='' />
-          </div>
+      </div>
+      <div className={styles.mainRight}>
+        <div className={styles.mainRightWrapper}>
+          <Post profileImg='https://images.unsplash.com/photo-1615789591457-74a63395c990?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZG9tZXN0aWMlMjBjYXR8ZW58MHx8MHx8&w=1000&q=80'
+            username='pistachio'
+            name='Pistachio The Cat'
+            mediaSrc='https://surfwaves.b-cdn.net/cat_video_example.mp4'
+            caption='Pistachio moves in silence #pistachio #trending #cats #waves'
+            soundCaption='pistachio'
+            soundSrc='' />
         </div>
+      </div>
     </div>
   )
 };
