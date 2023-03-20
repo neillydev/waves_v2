bf5cb04d2effcf0d5657bca0d1bdc09762fcbdca
neillydev <neillydev@gmail.com>
2023-03-19 18:05:18 -0700

    add login modal controls

--- src/components/LoginModal/LoginModal.tsx
+++ src/components/LoginModal/LoginModal.tsx
@@ -9,7 +9,24 @@ const LoginModal = () => {
   return (
     <div className={`${modalState ? styles.modalActive : ''} ${styles.modalContainer}`}>
         <div className={styles.modalWrapper} onClick={() => modalDispatch({type: false})}>
-            
+            <div className={styles.loginModalContainer}>
+                <div className={styles.loginModalWrapper}>
+                    <h2>Login to Waves</h2>
+                    <ul className={styles.loginList}>
+                        <li className={styles.loginItem}>
+                            <div className={styles.loginIcon}>
+
+                            </div>
+                            <h3>Login with email / username</h3>
+                        </li>
+                    </ul>
+                    <div className={styles.registerWrapper}>
+                        <h3>Don't have an account?</h3>
+                        <span className={styles.spanSeparator} />
+                        <span className={styles.registerSpan}>Register</span>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   )
