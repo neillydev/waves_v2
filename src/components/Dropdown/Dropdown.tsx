57c1bcebce850526e7b2b8163ed3b0b5a6bacfa4
neillydev <neillydev@gmail.com>
2023-03-30 02:18:21 -0700

    add Dropdown item icons and styling

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+
+import UserSVG from '../../../public/user.svg';
+import CogSVG from '../../../public/cog.svg';
+import LogOutSVG from '../../../public/logout.svg';
+
 import styles from '../../styles/Navbar/Navbar.module.css';
 
 const Dropdown = () => {
@@ -11,13 +16,16 @@ const Dropdown = () => {
             <li className={styles.dropHeader}>
             </li>
             <li className={styles.dropItem}>
+                <UserSVG />
                 <span>Profile</span>
             </li>
             <li className={styles.dropItem}>
+                <CogSVG />
                 <span>Settings</span>
             </li>
             <span className={styles.separator}></span>
             <li className={styles.dropItem}>
+                <LogOutSVG />
                 <span>Log Out</span>
             </li>
         </ul>
