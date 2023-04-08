e7773ea0df70f1430ce95fa1e7755458a8ff55e4
neillydev <neillydev@gmail.com>
2023-04-08 00:28:44 -0700

    add post deletion endpoint

---
+++ src/pages/api/post/[post_id].ts
@@ -0,0 +1,41 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+import formidable from "formidable";
+
+type PostResBody = {
+  message?: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<PostResBody>
+) {
+  const { method } = req;
+
+  switch (method) {
+    case "DELETE":
+      try {
+        const { post_id } = req.query;
+        const response = await axios({
+          method: "DELETE",
+          url: `http://localhost:8022/post/${post_id}`,
+        });
+
+        if (response.status === 201) {
+          return res.status(201);
+        } else {
+          return res.status(400).json({ message: "" });
+        }
+      } catch (e) {
+        return res.status(500).json({ message: "Internal server error" });
+      }
+      break;
+    default:
+      break;
+  }
+}
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
