b557c8b4c1f1e7640d767848c59f03c9f1822e52
neillydev <neillydev@gmail.com>
2023-04-23 18:40:54 -0700

    remove strictmode

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
