266d22ff25b47a211794424189707729a20f2b2b
neillydev <neillydev@gmail.com>
2023-04-25 20:10:19 -0700

    add query parameter handling for Explore page

--- src/pages/explore/index.tsx
+++ src/pages/explore/index.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import styles from "../../styles/Explore/Explore.module.css";
+import { useRouter } from "next/router";
 
 export enum ExploreViewType {
   TOP,
@@ -9,30 +10,57 @@ export enum ExploreViewType {
 }
 
 const Explore = () => {
-    const [viewType, setViewType] = useState<ExploreViewType>(ExploreViewType.TOP);
+  const router = useRouter();
+  const { q } = router.query;
+
+  const [viewType, setViewType] = useState<ExploreViewType>(
+    ExploreViewType.TOP
+  );
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const qParam = params.get('q');
+    if (!qParam) {
+      router.push("/");
+    }
+  }, [q, router]);
+
+  if (!q) {
+    return null;
+  }
 
   return (
     <div className={styles.exploreContainer}>
       <div className={styles.exploreWrapper}>
         <div className={styles.exploreTabsWrapper}>
           <ul className={styles.exploreTabsList}>
-            <li 
-            className={`${styles.exploreTabsItem} ${viewType !== ExploreViewType.TOP || styles.selected}`}
-            onClick={() => setViewType(ExploreViewType.TOP)}>
+            <li
+              className={`${styles.exploreTabsItem} ${
+                viewType !== ExploreViewType.TOP || styles.selected
+              }`}
+              onClick={() => setViewType(ExploreViewType.TOP)}
+            >
               <span>Top</span>
             </li>
-            <li 
-            className={`${styles.exploreTabsItem} ${viewType !== ExploreViewType.CREATORS || styles.selected}`}
-            onClick={() => setViewType(ExploreViewType.CREATORS)}>
+            <li
+              className={`${styles.exploreTabsItem} ${
+                viewType !== ExploreViewType.CREATORS || styles.selected
+              }`}
+              onClick={() => setViewType(ExploreViewType.CREATORS)}
+            >
               <span>Creators</span>
             </li>
-            <li 
-            className={`${styles.exploreTabsItem} ${viewType !== ExploreViewType.VIDEOS || styles.selected}`}
-            onClick={() => setViewType(ExploreViewType.VIDEOS)}>
+            <li
+              className={`${styles.exploreTabsItem} ${
+                viewType !== ExploreViewType.VIDEOS || styles.selected
+              }`}
+              onClick={() => setViewType(ExploreViewType.VIDEOS)}
+            >
               <span>Videos</span>
             </li>
           </ul>
         </div>
+        <div className={styles.exploreBody}></div>
       </div>
     </div>
   );
