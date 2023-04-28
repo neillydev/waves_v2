b1af0fe184c2e717b4b2b14c77e964263e9f8d56
neillydev <neillydev@gmail.com>
2023-04-28 01:28:39 -0700

    create timestamp formatting util function

---
+++ src/util/formatTimestamp.ts
@@ -0,0 +1,20 @@
+export default function formatTimestamp(timestamp: string) {
+    const date = new Date(timestamp);
+    const now = new Date();
+  
+    const diff = now.getTime() - date.getTime();
+    const seconds = Math.floor(diff / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+  
+    if (seconds < 60) {
+      return "Just now";
+    } else if (minutes < 60) {
+      return `${minutes}m ago`;
+    } else if (hours < 24) {
+      return `${hours}h ago`;
+    } else {
+      return `${days}d ago`;
+    }
+  }
